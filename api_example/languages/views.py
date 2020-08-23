bb7ab2888fc96849b8dbd0eb8c4f097abddba1a4
gregoryedmonds <50955914+gregoryedmonds@users.noreply.github.com>
2020-08-23 14:51:47 +0800

    Getting Started With Django REST Framework tutorial
    available from: https://www.youtube.com/watch?v=263xt_4mBNc

--- api_example/languages/views.py
+++ api_example/languages/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Language
+from .serializers import LanguageSerializer
 
-# Create your views here.
+class LanguageView(viewsets.ModelViewSet):
+    queryset = Language.objects.all()
+    serializer_class = LanguageSerializer
