fae73960ca9b8619121c321876ce6fd415aa7519
gregoryedmonds <50955914+gregoryedmonds@users.noreply.github.com>
2020-08-29 20:25:36 +0800

    fix

--- djangoAPI2/tester/urls.py
+++ djangoAPI2/tester/urls.py
@@ -1,15 +1,11 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from tester import views
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('users/', views.user_list), #all users
-    path('users/<int:pk>/profile/', views.user_detail), #user profile
-=======
-    path('users/', views.user_list),
-    path('users/<int:pk>/', views.user_detail),
->>>>>>> parent of 80aa7a7... minor changes
-    path('posts/', views.post_list),
-    path('posts/<int:pk>/', views.post_detail),
-    path('users/<int:pk>/posts/', views.user_post_list)
+    path('users/', views.UserList.as_view()),
+    path('users/<int:pk>/', views.UserDetail.as_view()),
+    path('posts/', views.PostList.as_view()),
+    path('posts/<int:pk>/', views.PostDetail.as_view()),
+    path('users/<int:pk>/posts/', views.UserPostList.as_view())
 ]
