d4e7423d41290b23cc6e0a1c18f969d8b23a0662
grae888 <22254857@student.uwa.edu.au>
2020-09-13 20:52:55 +0800

    comments
    added a few comments

--- BackEnd/LabShare/urls.py
+++ BackEnd/LabShare/urls.py
@@ -4,17 +4,9 @@ from rest_framework import routers
 from LabShare import views
 
 urlpatterns = [
-    #path('users/', views.UserList.as_view()), ##all users
-    #path('users/<int:pk>/', views.UserDetail.as_view()), ##user fact data
-    #path('current/', views.Current.as_view()), ##current user
-    #path('posts/', views.PostList.as_view()), ##all posts
-    #path('posts/<int:pk>/', views.PostDetail.as_view()), ##single post
-    #path('users/<int:author>/posts/', views.UserPostList.as_view()), ##posts for a user
-    #path('preferences/', views.Preferences.as_view()), ##create preference
-    #path('profile/', views.Profile.as_view()), ##create profile
-    path('user/<str:email>/preferences/', views.Preferences.as_view()),
-    path('user/current/', views.Current.as_view()),
-    path('user/<str:email>/feed/<int:id>/', views.SinglePost.as_view()),
+    path('user/<str:email>/preferences/', views.Preferences.as_view()), #user preferences
+    path('user/current/', views.Current.as_view()), #currently logged in user email
+    path('user/<str:email>/feed/<int:id>/', views.SinglePost.as_view()), #
     path('user/<str:email>/feed/', views.Feed.as_view()),
     path('user/<str:email>/posts/', views.UserPosts.as_view()),
     path('user/<str:email>/profile/', views.Profile.as_view()),
@@ -34,3 +26,9 @@ urlpatterns = [
 #current/       #current user
 #profile/       #add/remove profile
 #preferences/       #user preferences
+
+#automatically set preferences and profile on register
+#search function
+#change authentication to cookies
+#add categories and tags to posts
+#add page permissions
--- BackEnd/LabShare/views.py
+++ BackEnd/LabShare/views.py
@@ -39,6 +39,8 @@ class Profile(generics.GenericAPIView, mixins.RetrieveModelMixin, mixins.UpdateM
         return self.retrieve(request, *args, **kwargs)
     def put(self, request, *args, **kwargs):
         return self.update(request, *args, **kwargs)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
 class UserPosts(generics.GenericAPIView, mixins.ListModelMixin):
     lookup_field = 'author__email'
@@ -86,69 +88,3 @@ class Preferences(generics.GenericAPIView, mixins.RetrieveModelMixin, mixins.Cre
         return self.update(request, *args, **kwargs)
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
-
-##TESTING
-
-#class UserList(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
-#    queryset = User.objects.all()
-#    serializer_class = UserSerializer
-#    def get(self, request, *args, **kwargs):
-#        return self.list(request, *args, **kwargs)
-
-#class UserDetail(generics.GenericAPIView, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
-#    queryset = User.objects.all()
-#    serializer_class = UserSerializer
-#    def get(self, request, *args, **kwargs):
-#        return self.retrieve(request, *args, **kwargs)
-#    def post(self, request, *args, **kwargs):
-#        return self.update(request, *args, **kwargs)
-#    def delete(self, request, *args, **kwargs):
-#        return self.destroy(request, *args, **kwargs)
-
-#class Current(APIView):
-#    def get(self, request, format = None):
-#        return Response(request.user.email)
-
-#class PostList(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
-#    queryset = Post.objects.all()
-#    serializer_class = PostSerializer
-#    def perform_create(self, serializer):
-#        serializer.save(author = self.request.user)
-#    def get(self, request, *args, **kwargs):
-#        return self.list(request, *args, **kwargs)
-#    def post(self, request, *args, **kwargs):
-#        return self.create(request, *args, **kwargs)
-
-#class PostDetail(generics.GenericAPIView, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
-#    queryset = Post.objects.all()
-#    serializer_class = PostSerializer
-#    def get(self, request, *args, **kwargs):
-#        return self.retrieve(request, *args, **kwargs)
-#    def put(self, request, *args, **kwargs):
-#        return self.update(request, *args, **kwargs)
-#    def delete(self, request, *args, **kwargs):
-#        return self.destroy(request, *args, **kwargs)
-
-#class UserPostList(generics.GenericAPIView, mixins.ListModelMixin):
-#    def get_queryset(self):
-#        author = self.kwargs['author']
-#        return Post.objects.filter(author = author)
-#    serializer_class = PostSerializer
-#    def get(self, request, *args, **kwargs):
-#        return self.list(request, *args, **kwargs)
-
-#class Preferences(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
-#    queryset = UserPreferences.objects.all()
-#    serializer_class = UserPreferencesSerializer
-#    def get(self, request, *args, **kwargs):
-#        return self.list(request, *args, **kwargs)
-#    def post(self, request, *args, **kwargs):
-#        return self.create(request, *args, **kwargs)
-
-#class Profile(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
-#    queryset = UserProfile.objects.all()
-#    serializer_class = UserProfileSerializer
-#    def get(self, request, *args, **kwargs):
-#        return self.list(request, *args, **kwargs)
-#    def post(self, request, *args, **kwargs):
-#        return self.create(request, *args, **kwargs)
