6dd5ceb3292e1e94e782bd56cc4337e3e77b7bf3
grae888 <22254857@student.uwa.edu.au>
2020-09-23 13:53:33 +0800

    back to sqlite3

--- BackEnd/BackEnd/settings.py
+++ BackEnd/BackEnd/settings.py
@@ -76,14 +76,11 @@ WSGI_APPLICATION = 'BackEnd.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
