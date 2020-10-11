81cc91eeed5aaef3c6183c274213a15e618736c7
gregoryedmonds <50955914+gregoryedmonds@users.noreply.github.com>
2020-10-11 23:00:01 +0800

    Reset password complete
    working fully with email on backend

--- BackEnd/BackEnd/settings.py
+++ BackEnd/BackEnd/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'BackEnd.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- BackEnd/LabShare/models.py
+++ BackEnd/LabShare/models.py
@@ -96,9 +96,22 @@ def create_profile(sender, instance=None, created=False, **kwargs):
 @receiver(reset_password_token_created)
 def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
 
-    email_plaintext_message = "{}?token={}".format(reverse('password_reset:reset-password-request'), reset_password_token.key)
+    # send an e-mail to the user
+    context = {
+        'current_user': reset_password_token.user,
+        'username': reset_password_token.user.username,
+        'email': reset_password_token.user.email,
+        'token': reset_password_token.key,
+        'reset_password_url': "{}?token={}".format(
+            instance.request.build_absolute_uri(reverse('password_reset:reset-password-confirm')),
+            reset_password_token.key)
+    }
 
-    send_mail(
+    # render email text
+    email_html_message = render_to_string('email/user_reset_password.html', context)
+    email_plaintext_message = render_to_string('email/user_reset_password.txt', context)
+
+    msg = EmailMultiAlternatives(
         # title:
         "Password Reset for {title}".format(title="LabShare App"),
         # message:
@@ -108,3 +121,5 @@ def password_reset_token_created(sender, instance, reset_password_token, *args, 
         # to:
         [reset_password_token.user.email]
     )
+    msg.attach_alternative(email_html_message, "text/html")
+    msg.send()
