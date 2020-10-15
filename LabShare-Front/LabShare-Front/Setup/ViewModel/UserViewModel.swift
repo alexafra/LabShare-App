1994f4cbd63fa29181c9a9e8fd6ad329e9e04d5d
LiamNied <22499153@student.uwa.edu.au>
2020-10-15 21:14:21 +0800

    Comment Out to make buildable

--- LabShare-Front/LabShare-Front/Setup/ViewModel/UserViewModel.swift
+++ LabShare-Front/LabShare-Front/Setup/ViewModel/UserViewModel.swift
@@ -15,45 +15,45 @@ class UserViewModel: ObservableObject {
         self.user = user
     }
     
-    func updateProfile(userAuthVM: UserAuthenticationViewModel) {
+    func updateIsActive(userAuthVM: UserAuthenticationViewModel) {
          //For register -> Profile edit view
         
         if !userAuthVM.userAuth.isLoggedIn {
             userAuthVM.userAuth.isLoggedIn = true
         }
         
-        let profileWebService = ProfileWebService(userAuth: userAuthVM.userAuth)
-        profileWebService.updateProfile(profileModel: self.profile, completionFailure: {() -> Void in
-//            self.hasCompletedLoading = true
-//            self.loadingSuccessful = false
-            
-            
-            return
-            
-        }, completionSuccessful: { (profile: ProfileModel?) -> Void in
-            if let profile = profile {
-                self.profile = profile
-            }
-            
-            return
-        })
-        
-        let userWebService = UserWebService(userAuth: userAuthVM.userAuth)
-        userWebService.updateUser(userId: self.profile.owner.id, userModel: self.profile.owner, completionFailure: {() -> Void in
-//            self.hasCompletedLoading = true
-//            self.loadingSuccessful = false
-            
-            
-            return
-            
-        }, completionSuccessful: { (user: UserModel?) -> Void in
-            if let user = user {
-                self.profile.owner = user
-            }
-            
-            return
-        })
-            // //////////NEED TO REPLACE self.userAuthVM.userAuth.isLoggedIn = true
+//        let profileWebService = ProfileWebService(userAuth: userAuthVM.userAuth)
+//        profileWebService.updateProfile(profileModel: self.profile, completionFailure: {() -> Void in
+////            self.hasCompletedLoading = true
+////            self.loadingSuccessful = false
+//
+//
+//            return
+//
+//        }, completionSuccessful: { (profile: ProfileModel?) -> Void in
+//            if let profile = profile {
+//                self.profile = profile
+//            }
+//
+//            return
+//        })
+//
+//        let userWebService = UserWebService(userAuth: userAuthVM.userAuth)
+//        userWebService.updateUser(userId: self.profile.owner.id, userModel: self.profile.owner, completionFailure: {() -> Void in
+////            self.hasCompletedLoading = true
+////            self.loadingSuccessful = false
+//
+//
+//            return
+//
+//        }, completionSuccessful: { (user: UserModel?) -> Void in
+//            if let user = user {
+//                self.profile.owner = user
+//            }
+//
+//            return
+//        })
+//            // //////////NEED TO REPLACE self.userAuthVM.userAuth.isLoggedIn = true
     }
     
 }
